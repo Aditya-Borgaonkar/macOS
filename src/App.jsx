755762d4afa88e264f394fd519063f1fd36dd8be
Aditya-Borgaonkar <adityaborgaonkar4@gmail.com>
2026-01-19 23:59:10 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './app.scss'
+import Dokes from './components/Dokes'
+function App() {
+  return (
+    <>
+      <main>
+        <Dokes />
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Dokes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dokes = () => {
+    return (
+        <Footer>
+
+        </Footer>
+    )
+}
+
+export default Dokes
