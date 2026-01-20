ce3236e604deb3e702461736e19929e5cc7cbf35
Aditya-Borgaonkar <adityaborgaonkar4@gmail.com>
2026-01-20 23:04:36 +0530

    Dock

--- src/components/Dokes.jsx
+++ src/components/Dokes.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import "./dock.scss"
 
 const Dokes = () => {
     return (
-        <Footer>
-
-        </Footer>
+        <footer className='dock'>
+            <div className="icon github"><img src="/dokes-icons/github.svg" alt="" /></div>
+            <div className="icon note"><img src="/dokes-icons/note.svg" alt="" /></div>
+            <div className="icon pdf"><img src="/dokes-icons/pdf.svg" alt="" /></div>
+            <div className="icon calender"><img src="/dokes-icons/calender.svg" alt="" /></div>
+            <div className="icon spotify"><img src="/dokes-icons/spotify.svg" alt="" /></div>
+            <div className="icon mail"><img src="/dokes-icons/mail.svg" alt="" /></div>
+            <div className="icon link"><img src="/dokes-icons/link.svg" alt="" /></div>
+            <div className="icon cli"><img src="/dokes-icons/cli.svg" alt="" /></div>
+        </footer>
     )
 }
 
