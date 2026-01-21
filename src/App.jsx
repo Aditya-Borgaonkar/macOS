f0a4ab9b83743b01d03fc4ab66dc8f9704f3a8f3
Aditya-Borgaonkar <adityaborgaonkar4@gmail.com>
2026-01-21 16:14:48 +0530

    Github Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import './app.scss'
 import Dokes from './components/Dokes'
 import Nav from './components/Nav'
+import GitHub from './components/windows/GitHub'
+
 function App() {
   return (
     <>
       <main>
         <Nav />
         <Dokes />
+        <GitHub />
       </main>
     </>
   )
---
+++ src/components/windows/GitHub.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import githubData from '../../assets/github.json'
+import MacWindow from './MacWindow'
+import './github.scss'
+
+const Gitcard = ({ data = { id: 1, image: "", title: "", description: "", tags: [], repoLink: "", demoLink: "" } }) => {
+    return <div className="card">
+        <img src={data.image} />
+        <h3>{data.title}</h3>
+        <p className='description'>{data.description}</p>
+        <div className="tags">
+            {data.tags.map(tag => <p className='tag'>{tag}</p>)}
+        </div>
+        <div className="links">
+            <a href={data.repoLink}>Repo</a>
+            {data.demoLink && <a href={data.demoLink}>Demo</a>}
+        </div>
+    </div>
+}
+
+const GitHub = () => {
+    return (
+        <MacWindow>
+            <div className="cards">
+                {githubData.map(project => {
+                    return <Gitcard data={project} />
+                })}
+            </div>
+        </MacWindow>
+    )
+}
+
+export default GitHub
